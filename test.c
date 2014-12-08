33eb22a1e53049a2989a9cc60ba79f97594b1bcc
Chen, Yiqun <feedingfranzy@gmail.com>
2014-12-08 16:34:21 +0100

    Covering test program
    The initial version of a sound funcational test program. With some
    trials it may cause the fifo the be fulfilled or left empty.
    
    Signed-off-by: Chen, Yiqun

--- test.c
+++ test.c
@@ -7,19 +7,21 @@
 
 #define TEST_STR "This is test\n"
 
-static void sig_chld(int signo)
+int term = 0;
+
+void sig_chd(int signo)
 {
-	_exit(0);
+	term = 1;
 }
 
 int main()
 {
 	int fd_r, fd_w;
-	pid_t pid;
 	char *buf;
+	pid_t pid;
+	struct sigaction sigact;
 	int retval;
 
-	buf = (char *)malloc(BUFSIZ);
 	fd_w = open("/dev/fifo0", O_WRONLY);
 	fd_r = open("/dev/fifo1", O_RDONLY);
 	if (fd_w < 0)
@@ -27,12 +29,20 @@ int main()
 	if (fd_r < 0)
 		fprintf(stderr, "Error opening fifo1");
 
+	sigact.sa_handler = sig_chd;
+	/* We don't care if the old sigmask will be 
+	 * reset 
+	 */
+	sigaction(SIGCHLD, &sigact, NULL);
+
 	pid = fork();
 	if (pid < 0)
 		return 1;
 	else if (!pid) { 
 		/* child */
 		int i = 0;
+		buf = (char *)malloc(BUFSIZ);
+
 		for (i = 0; i < 4096; i++) {
 			retval = write(fd_w, TEST_STR, strlen(TEST_STR) + 1);
 			if (retval < 0) {
@@ -40,27 +50,50 @@ int main()
 				return retval;
 			}
 		}
+		memset(buf, (int)'y', BUFSIZ);
+		retval = write(fd_w, buf, BUFSIZ);
+		if (retval < 0) {
+			perror("Error writing\n");
+			return retval;
+		} else if (retval < BUFSIZ) {
+			for (i = 0; i < 3; i++)
+				retval = write(fd_w, buf, BUFSIZ);
+		}
 		exit(0);
 	} else {
 		/* parent */
 		int i = 0;
-		const struct sigaction sig_child = {
-			.sa_handler = sig_chld,
-		};
+		buf = (char *)malloc(BUFSIZ);
 
-		sigaction(SIGCHLD, &sig_child, NULL);
 		for (i = 0; i < 4096; i++) {
-			retval = read(fd_r, buf, BUFSIZ);
-			if (retval < 0) {
-				perror("Error reading\n");
-				return retval;
+			if (!term) {
+				retval = read(fd_r, buf, BUFSIZ);
+				if (retval < 0) {
+					perror("Error reading\n");
+					return retval;
+				}
+				retval = write(STDOUT_FILENO, buf, BUFSIZ);
+				if (retval < 0) {
+					perror("Cannot print!\n");
+					return retval;
+				}
+				bzero(buf, BUFSIZ);
+				sleep(rand() % 10);
+			} else {
+				if (!strlen(buf)) {
+					retval = read(fd_r, buf, BUFSIZ);
+					if (retval < 0) {
+						perror("Cannot read from fifo!\n");
+						return retval;
+					}
+				}
+				retval = write(STDOUT_FILENO, buf, BUFSIZ);
+				if (retval < 0) {
+					perror("Cannot print!\n");
+					return retval;
+				}
+				return 0;
 			}
-			retval = write(STDOUT_FILENO, buf, BUFSIZ);
-			if (retval < 0) {
-				perror("Cannot print!\n");
-				return retval;
-			}
-			bzero(buf, BUFSIZ);
 		}
 
 		waitpid(pid, NULL, 0);
