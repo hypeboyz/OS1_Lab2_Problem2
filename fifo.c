02ad80c786bdd15d68e341dd7ef3fa85732a527e
Yiqun Chen <feedingfranzy@gmail.com>
2017-11-13 10:57:43 +0100

    Fix of driver bugs
    1. Only node_num / 2 devices are available. Modify it.
    
    2. Previously I tested empty and full condition via pointers which does not
    make sense when the buffer is circular due to the off-by-one problem.
    Now the empty/full condition is tested against size

--- fifo.c
+++ fifo.c
@@ -1,12 +1,10 @@
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/semaphore.h>
 #include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/semaphore.h>
-#include <linux/uaccess.h>
 #include <linux/sched.h>
+#include <linux/uaccess.h>
 #include <linux/slab.h>
+#include <linux/semaphore.h>
 #include "fifo.h"
 
 MODULE_LICENSE("GPL");
@@ -19,10 +17,10 @@ int fifo_open(struct inode *inode, struct file *filp)
 	struct fifo_dev *dev;
 
 	if (IS_EVEN(iminor(inode)) &&
-			((filp->f_flags & O_ACCMODE) != O_WRONLY))
+	    ((filp->f_flags & O_ACCMODE) != O_WRONLY))
 		return -EPERM;
 	else if (IS_ODD(iminor(inode)) &&
-			((filp->f_flags & O_ACCMODE) != O_RDONLY))
+		 ((filp->f_flags & O_ACCMODE) != O_RDONLY))
 		return -EPERM;
 
 	dev = container_of(inode->i_cdev, struct fifo_dev, f_cdev);
@@ -34,16 +32,17 @@ int fifo_open(struct inode *inode, struct file *filp)
 /* This function is not reentrant
  * NEVER NEVER call it outside fifo_read()
  */
-static ssize_t __do_read(struct fifo_dev *dev, char __user *buf,
-		size_t count)
+static inline ssize_t __do_read(struct fifo_dev *dev, char __user *buf,
+				size_t count)
 {
 	count = min(dev->f_size, count);
-	if (dev->f_wp == dev->f_rp)
+	if (is_empty(dev)) {
 		return 0;
-	else if (dev->f_wp > dev->f_rp)
+	} else if (dev->f_wp > dev->f_rp) {
 		count = min(count, (size_t)(dev->f_wp - dev->f_rp));
-	else
+	} else {
 		count = min(count, (size_t)(dev->f_end - dev->f_rp));
+	}
 
 	if (copy_to_user(buf, dev->f_rp, count))
 		return -EFAULT;
@@ -51,22 +50,20 @@ static ssize_t __do_read(struct fifo_dev *dev, char __user *buf,
 	dev->f_rp += count;
 	if (dev->f_rp == dev->f_end)
 		dev->f_rp = dev->f_data;
-	if (dev->f_wp == dev->f_end)
-		dev->f_wp = dev->f_data;
 
 #ifdef DEBUG
 	if (unlikely(dev->f_rp > dev->f_end)) {
-		printk(KERN_EMERG"Error occured on reading!\n"
-				"dev->f_rp = %lx, dev->f_end = %lx\n",
-				(unsigned long)dev->f_rp,
-				(unsigned long)dev->f_end);
+		pr_emerg("Error occured on reading!\n"
+			 "dev->f_rp = 0x%lx, dev->f_end = 0x%lx\n",
+			 (unsigned long)dev->f_rp,
+			 (unsigned long)dev->f_end);
 		return -EFAULT;
 	}
 	if (unlikely(dev->f_rp < dev->f_data)) {
-		printk(KERN_EMERG"Error occured on reading!\n"
-				"dev->f_rp = %lx, dev->f_data = %lx\n",
-				(unsigned long)dev->f_rp,
-				(unsigned long)dev->f_data);
+		pr_emerg("Error occured on reading!\n"
+			 "dev->f_rp = 0x%lx, dev->f_data = 0x%lx\n",
+			 (unsigned long)dev->f_rp,
+			 (unsigned long)dev->f_data);
 		return -EFAULT;
 	}
 #endif
@@ -75,7 +72,7 @@ static ssize_t __do_read(struct fifo_dev *dev, char __user *buf,
 }
 
 ssize_t fifo_read(struct file *filp, char __user *buf,
-		size_t count, loff_t *offp)
+		  size_t count, loff_t *offp)
 {
 	ssize_t retval = 0;
 	struct fifo_dev *dev;
@@ -85,32 +82,38 @@ ssize_t fifo_read(struct file *filp, char __user *buf,
 	if (down_interruptible(&dev->f_sem))
 		return -ERESTARTSYS;
 #ifdef DEBUG
-	if (is_empty(dev))
-		pr_debug("The fifo is empty\n");
+	if (is_empty(dev)) {
+		pr_debug("The fifo is empty\n"
+			 "dev->f_rp = 0x%lx, dev->f_wp = 0x%lx\n"
+			 "dev->f_data = 0x%lx, dev->f_end = 0x%lx\n"
+			 "dev->f_size = %lu\n",
+			 (unsigned long)dev->f_rp,
+			 (unsigned long)dev->f_wp,
+			 (unsigned long)dev->f_data,
+			 (unsigned long)dev->f_end,
+			 (unsigned long)dev->f_size);
+	}
 #endif
 
 	while (is_empty(dev)) {
 		up(&dev->f_sem);
 		if (filp->f_flags & O_NONBLOCK)
 			return -EAGAIN;
-		if (wait_event_interruptible(dev->f_inq, !is_empty(dev)) < 0)
+		if (wait_event_interruptible(dev->f_outq, !is_empty(dev)) < 0)
 			return -ERESTARTSYS;
 		if (down_interruptible(&dev->f_sem))
 			return -ERESTARTSYS;
 	}
 
 	retval = __do_read(dev, buf, count);
-	if (retval < 0)
-		goto out;
-	wake_up_interruptible(&dev->f_outq);
 
-out:
 	up(&dev->f_sem);
+	wake_up_interruptible(&dev->f_inq);
 	return retval;
 }
 
-static ssize_t __do_write(struct fifo_dev *dev, const char __user *buf,
-		size_t count)
+static inline ssize_t __do_write(struct fifo_dev *dev, const char __user *buf,
+				 size_t count)
 {
 	if (is_full(dev))
 		return 0;
@@ -119,32 +122,36 @@ static ssize_t __do_write(struct fifo_dev *dev, const char __user *buf,
 	else
 		count = min(count, (size_t)(dev->f_rp - dev->f_wp));
 
-	if (copy_from_user(dev->f_wp, buf, count))
+	if (copy_from_user(dev->f_wp, buf, count)) {
 		return -EFAULT;
+	}
 	dev->f_size += count;
 	dev->f_wp += count;
-	if ((dev->f_wp == dev->f_end) && (dev->f_rp != dev->f_data))
+	if ((dev->f_wp == dev->f_end))
 		dev->f_wp = dev->f_data;
 
 #ifdef DEBUG
 	if (unlikely(dev->f_wp > dev->f_end )) {
-		printk(KERN_EMERG"Error occured on writing!\n"
-				"dev->f_wp = %lx, dev->f_end = %lx\n",
-				(unsigned long)dev->f_wp,
-				(unsigned long)dev->f_end);
+		pr_emerg("Error occured on writing!\n"
+			 "dev->f_wp = 0x%lx, dev->f_end = 0x%lx\n",
+			 (unsigned long)dev->f_wp,
+			 (unsigned long)dev->f_end);
+		return -EFAULT;
 	}
 	if (unlikely(dev->f_wp < dev->f_data)) {
-		printk(KERN_EMERG"Error occured on writing!\n"
-				"dev->f_wp = %lx, dev->f_data = %lx\n",
-				(unsigned long)dev->f_wp,
-				(unsigned long)dev->f_data);
+		pr_emerg("Error occured on writing!\n"
+			 "dev->f_wp = 0x%lx, dev->f_data = 0x%lx\n",
+			 (unsigned long)dev->f_wp,
+			 (unsigned long)dev->f_data);
+		return -EFAULT;
 	}
 #endif
 
 	return count;
 }
-ssize_t fifo_write(struct file *filp, const char __user *buf,
-		size_t count, loff_t *offp)
+
+static ssize_t fifo_write(struct file *filp, const char __user *buf,
+			  size_t count, loff_t *offp)
 {
 	ssize_t retval = 0;
 	struct fifo_dev *dev;
@@ -152,30 +159,37 @@ ssize_t fifo_write(struct file *filp, const char __user *buf,
 	dev = filp->private_data;
 
 	if (down_interruptible(&dev->f_sem)) {
-		printk(KERN_EMERG"write cannot get the first lock!\n");
+		pr_emerg("write cannot get the first lock!\n");
 		return -ERESTARTSYS;
 	}
 #ifdef DEBUG
-	if (is_full(dev))
-		pr_debug("The fifo is full\n");
+	if (is_full(dev)){
+		pr_debug("The fifo is full\n"
+			 "dev->f_rp = 0x%lx, dev->f_wp = 0x%lx\n"
+			 "dev->f_data = 0x%lx, dev->f_end = 0x%lx\n"
+			 "dev->f_size = %lu\n",
+			 (unsigned long)dev->f_rp,
+			 (unsigned long)dev->f_wp,
+			 (unsigned long)dev->f_data,
+			 (unsigned long)dev->f_end,
+			 (unsigned long)dev->f_size);
+	}
 #endif
 
 	while (is_full(dev)) {
 		up(&dev->f_sem);
 		if (filp->f_flags & O_NONBLOCK)
 			return -EAGAIN;
-		if (wait_event_interruptible(dev->f_outq, !is_full(dev)) < 0)
+		if (wait_event_interruptible(dev->f_inq, !is_full(dev)) < 0)
 			return -ERESTARTSYS;
 		if (down_interruptible(&dev->f_sem))
 			return -ERESTARTSYS;
 	}
 
 	retval = __do_write(dev, buf, count);
-	if (retval < 0)
-		return -EFAULT;
 
-	wake_up_interruptible(&dev->f_inq);
 	up(&dev->f_sem);
+	wake_up_interruptible(&dev->f_outq);
 	return retval;
 }
 
@@ -186,19 +200,22 @@ static const struct file_operations fifo_ops = {
 	.write = fifo_write
 };
 
-static int fifo_setup_dev(struct fifo_dev *dev, int index)
+static int fifo_setup_dev(struct fifo_dev *dev, const int index)
 {
 	int err = 0;
-	dev_t devno;
+	dev_t devno_in;
+	dev_t devno_out;
 
 	if (down_interruptible(&dev->f_sem))
 		return -ERESTARTSYS;
-	devno = MKDEV(fifo_major, fifo_minor + (index << 1));
+	devno_in = MKDEV(fifo_major, fifo_minor + (index << 1));
+	devno_out = MKDEV(fifo_major, fifo_minor + (index << 1) + 1);
 	cdev_init(&dev->f_cdev, &fifo_ops);
-	err = cdev_add(&dev->f_cdev, devno, 2);
+	err = cdev_add(&dev->f_cdev, devno_in, 1);
+	err = cdev_add(&dev->f_cdev, devno_out, 1);
 	if (err < 0)
 		goto out;
-	dev->f_data = (char *)__get_free_pages(GFP_KERNEL, ORDER);
+	dev->f_data = (char *)kmalloc(PAGE_SIZE << ORDER, GFP_KERNEL);
 	dev->f_end = dev->f_data + (PAGE_SIZE << ORDER);
 	dev->f_size = 0;
 	dev->f_rp = dev->f_data;
@@ -216,12 +233,13 @@ static int __init fifo_init(void)
 	int err = 0;
 	int i = 0;
 
-	err = alloc_chrdev_region(&fifo_devno, 0, fifo_nr_devs, "fifo");
+	err = alloc_chrdev_region(fifo_devno, 0, fifo_nr_devs, "fifo");
 	if (err < 0)
 		return err;
-	fifo_major = MAJOR(fifo_devno);
+	fifo_major = MAJOR(fifo_devno[0]);
 	for (i = 0; i < fifo_nr_devs; i++) {
 		sema_init(&fifo_dev[i].f_sem, 1);
+		mutex_init(&fifo_dev[i].f_mtx);
 		err = fifo_setup_dev(&fifo_dev[i], i);
 		if (err < 0)
 			return err;
@@ -234,10 +252,10 @@ static void __exit fifo_exit(void)
 	int i = 0;
 
 	for (i = 0; i < fifo_nr_devs; i++) {
-		free_pages((unsigned long)(fifo_dev[i].f_data), ORDER);
+		kfree(fifo_dev[i].f_data);
+		unregister_chrdev_region(fifo_devno[i], fifo_nr_devs);
 		cdev_del(&fifo_dev[i].f_cdev);
 	}
-	unregister_chrdev_region(fifo_devno, fifo_nr_devs);
 }
 
 module_init(fifo_init);
--- fifo.h
+++ fifo.h
@@ -1,5 +1,7 @@
 #ifndef FIFO_H
 #define FIFO_H
+#include <linux/cdev.h>
+#include <linux/mutex.h>
 
 #define IS_ODD(x) (x & 1)
 #define IS_EVEN(x) !(x & 1)
@@ -15,6 +17,7 @@
 struct fifo_dev {
 	wait_queue_head_t f_inq, f_outq;
 	struct semaphore f_sem;
+	struct mutex f_mtx;
 	struct cdev f_cdev;
 	size_t f_size;
 	char *f_data, *f_end;
@@ -23,18 +26,18 @@ struct fifo_dev {
 
 static inline int is_empty(struct fifo_dev *dev)
 {
-	return (dev->f_rp == dev->f_wp);
+	/* return (dev->f_rp == dev->f_wp); */
+	return !dev->f_size;
 }
 
 static inline int is_full(struct fifo_dev *dev)
 {
-	if ((dev->f_wp + 1) == dev->f_end)
-		return (dev->f_rp == dev->f_data);
-	if ((dev->f_wp == dev->f_end) && (dev->f_rp == dev->f_data))
-		return 1;
-	else if ((dev->f_wp == dev->f_end) && (dev->f_rp != dev->f_data))
-		dev->f_wp = dev->f_data;
-	return ((dev->f_wp + 1) == dev->f_rp);
+	/* if (dev->f_wp > dev->f_rp) { */
+	/* 	return (dev->f_wp == dev->f_end) && (dev->f_rp == dev->f_data); */
+	/* } else { */
+	/* 	return ((dev->f_wp + 1) == dev->f_rp); */
+	/* } */
+	return (PAGE_SIZE << ORDER) == dev->f_size;
 }
 
 static inline size_t free_space(struct fifo_dev *dev)
@@ -44,7 +47,7 @@ static inline size_t free_space(struct fifo_dev *dev)
 
 static struct fifo_dev fifo_dev[NR_DEVS >> 1];
 static int fifo_nr_devs = NR_DEVS >> 1;
-static dev_t fifo_devno;
+static dev_t fifo_devno[NR_DEVS >> 1];
 static dev_t fifo_major;
 static dev_t fifo_minor;	/* Minor number of the first device */
 
